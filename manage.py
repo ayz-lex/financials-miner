a7620d4db22ed6ee3a4016cd503dca6c36a153c7
Andrew <andrew@andrew.fios-router.home>
2021-01-04 15:59:27 -0500

    fix code syntax

--- manage.py
+++ manage.py
@@ -2,36 +2,41 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import time
 import random
 
 filename = "input.csv"
 
 data = []
 
-names = []
+# 7 ipo year
 
 with open(filename) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        if row[2] == 'n/a' or row[3] == '0' or (len(row[0]) > 4):
+        if row[2] == 'n/a' or row[3] == '0' or (len(row[0]) > 4) or float(row[5]) < 300000000:
             pass
         else:
-            names.append(row[0])
+            data.append([row[0], row[7]])
 
-for n in names:
+i = 0
+while i < len(data):
+
+    row = data[i]
+
+    n = row[0]
 
     r = None
     status = True
     url = f'https://finance.yahoo.com/quote/{n}/key-statistics?p={n}'
-    i = 0
 
-    while status and i < 3:
-        time.sleep(random.uniform(0, 1))
-        r = requests.get(url)
+    r = requests.get(url, headers = {'User-agent': 'Super Bot Power Level Over 9000'})
 
-        status = not (r.status_code == 200)
-        i += 1
+    # while status and i < 3:
+    #     time.sleep(random.uniform(0, 1))
+    #     r = requests.get(url)
+
+    #     status = not (r.status_code == 200)
+    #     i += 1
 
     try:
         text = r.text
@@ -40,7 +45,7 @@ for n in names:
         parsedData = soup.find(string="Valuation Measures").parent.parent.parent.parent
 
         dataContents = parsedData.contents
-        valuationData = dataContents[0].find('table')
+        #valuationData = dataContents[0].find('table')
         fiscalHighlights = dataContents[2].find_all(class_="Pos(r)")
         tradingInformation = dataContents[1].find_all(class_="Pos(r)")
 
@@ -49,20 +54,37 @@ for n in names:
         # 'Book Value/Share', 'Market Cap'
 
         qrg = fiscalHighlights[3].find(string="Quarterly Revenue Growth").parent.parent.next_sibling.string
-        ps = valuationData.find(string="Price/Sales").parent.parent.next_sibling.string
-        pe = valuationData.find(string="Trailing P/E").parent.parent.next_sibling.string
-        pef = valuationData.find(string="Forward P/E").parent.parent.next_sibling.string
+        #ps = valuationData.find(string="Price/Sales").parent.parent.next_sibling.string
+        #pe = valuationData.find(string="Trailing P/E").parent.parent.next_sibling.string
+        #pef = valuationData.find(string="Forward P/E").parent.parent.next_sibling.string
         bv = fiscalHighlights[4].find(string="Book Value Per Share").parent.parent.next_sibling.string
         profit = fiscalHighlights[1].find(string="Profit Margin").parent.parent.next_sibling.string
         change = tradingInformation[0].find(string="52-Week Change").parent.parent.next_sibling.string
-        mc = valuationData.find(string="Market Cap (intraday)").parent.parent.next_sibling.string
+        #mc = valuationData.find(string="Market Cap (intraday)").parent.parent.next_sibling.string
 
-        data.append([n, qrg, ps, pe, pef, bv, profit, change, mc])
-
+        #data.append([n, qrg, ps, pe, pef, bv, profit, change, mc])
+        row.extend([qrg, bv, profit, change])
+        i += 1
     except:
-        
-        print(f'Error on stock: {n}')
+        del data[i]
+        print(f'Error: {n}')
 
-df = pd.DataFrame(data, columns=['Ticker', 'Quarterly Revenue Growth', 'Price/Sales', 'Trailing P/E', 'Forward P/E', 'Book Value', 'Profit Margin', '52-Week Change', 'Market Cap'])
+def sort_func(element):
+    if element[2] == 'N/A':
+        return float('inf')
+    else:
+        res = 0.0
+
+        try:
+            res = float(element[2][:-1])
+        except:
+            res = float('inf')
+
+        return res
+
+data.sort(key = sort_func)
+
+#df = pd.DataFrame(data, columns=['Ticker', 'Quarterly Revenue Growth', 'Price/Sales', 'Trailing P/E', 'Forward P/E', 'Book Value', 'Profit Margin', '52-Week Change', 'Market Cap'])
+df = pd.DataFrame(data, columns=['Ticker', 'IPO Year', 'Quarterly Revenue Growth', 'Book Value', 'Profit Margin', '52-Week Change'])
 
 df.to_csv('output.csv')
